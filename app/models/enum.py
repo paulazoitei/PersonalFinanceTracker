66b17f81628e9e70e715cbbee98077c680841df8
paulazoitei <azoitei_paul@yahoo.com>
2025-09-18 15:44:03 +0300

    add_transaction_model_and_enum_in_it

---
+++ app/models/enum.py
@@ -0,0 +1,5 @@
+import enum
+
+class TypeEnum(enum.Enum):
+    EXPENSE="expense"
+    INCOME="income"
--- app/models/user.py
+++ app/models/user.py
@@ -3,7 +3,9 @@ from datetime import datetime
 
 class User(db.Model):
     __tablename__='user_data'
+    user_id=db.Column(db.Integer,primary_key=True)
     email=db.Column(db.String(50),nullable=False,unique=True)
+    password_hash=db.Column(db.String(100),nullable=False)
     base_currency=db.Column(db.String(3),nullable=False)
-    created_at=db.Column(db.Integer,default=lambda: datetime.now(),nullable=False)
+    created_at=db.Column(db.datetime,default=lambda: datetime.now(),nullable=False)
     
